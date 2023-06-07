0d32f290f27656f04e04a50a0e6c5772ef0683a7
Bogdan <kryptonumstudio@gmail.com>
2023-06-07 17:46:59 +0200

    continue case study page

--- src/components/sections/case-result-cards/index.js
+++ src/components/sections/case-result-cards/index.js
@@ -1,22 +1,29 @@
 import React from "react"
 import styles from "./styles.module.scss"
+import { Butterfly } from "../../../assets/butterfly"
+import { Diamond } from "../../../assets/diamond"
 
 export default function Cards({ data: { title, leftColumnTitle, leftColumnList, rightColumnTitle, rightColumnList } }) {
   return (
     <section className={styles.wrapper}>
       <div dangerouslySetInnerHTML={{ __html: title }} />
-
       <div className={styles.grid}>
         <div className={styles.left}>
           <h3 dangerouslySetInnerHTML={{ __html: leftColumnTitle }} />
           {leftColumnList.map((el, index) => (
-            <p key={index} dangerouslySetInnerHTML={{ __html: el.text }} />
+            <div className={styles.item} key={index} >
+              <Butterfly />
+              <p dangerouslySetInnerHTML={{ __html: el.text }} />
+            </div>
           ))}
         </div>
         <div className={styles.right}>
           <h3 dangerouslySetInnerHTML={{ __html: rightColumnTitle }} />
           {rightColumnList.map((el, index) => (
-            <p key={index} dangerouslySetInnerHTML={{ __html: el.text }} />
+            <div className={styles.item} key={index}>
+              <Diamond />
+              <p dangerouslySetInnerHTML={{ __html: el.text }} />
+            </div>
           ))}
         </div>
       </div>
