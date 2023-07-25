43d4a0f21538a0f48a8547ef7d1fcae0e59cdbcf
kryptonumstudio <kryptonumstudio@gmail.com>
2023-07-25 22:27:43 +0200

    small fixes

--- src/components/moleculas/product-card/index.js
+++ src/components/moleculas/product-card/index.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from "react"
+import React, { useCallback, useMemo, useState } from "react"
 import styles from './styles.module.scss'
 import AddToCart from "@/components/atoms/add-to-cart-button/index.js"
 import Link from "next/link"
@@ -10,6 +10,30 @@ import { CheckMark } from "../../../assets/check-mark"
 export const Card = ({ product }) => {
 
   const [chosenVariation, setChosenVariation] = useState(product?.variations?.nodes[0] || null)
+  const [chosenAddon, setChosenAddon] = useState(null)
+
+  const prices = useMemo(() => {
+    let localprice = chosenVariation?.price || product.price
+    let localregularPrice = chosenVariation?.regularPrice || product.regularPrice
+
+    if (!chosenAddon) return { price: parseInt(localprice), regularPrice: parseInt(localregularPrice) }
+
+    const newPrice = parseInt(localprice) + parseInt(chosenAddon.price)
+    const newRegularPrice = parseInt(localregularPrice) + parseInt(chosenAddon.price)
+
+    return { price: newPrice, regularPrice: newRegularPrice }
+
+  }, [product.price, product.regularPrice, chosenAddon, chosenVariation])
+
+  const handleAddonClick = useCallback((e, add) => {
+    //if its addon already chosen, remove it
+    if (chosenAddon?.val === add?.val) {
+      setChosenAddon(null)
+      return
+    }
+
+    setChosenAddon(add)
+  }, [chosenAddon, setChosenAddon])
 
   return (
     <div className={styles.wrapper}>
@@ -40,7 +64,7 @@ export const Card = ({ product }) => {
               </div>
             ))}
           </div>
-        ) : (
+        ) : product?.product?.bundleItems ? (
           <ul className={styles.bundle}>
             {product?.product?.bundleItems?.map(el => (
               <li key={el.text}>
@@ -49,11 +73,27 @@ export const Card = ({ product }) => {
               </li>
             ))}
           </ul>
-        )}
+        ) : product.addons ? (
+          <div className={styles.variables}>
+            {product.addons?.map((el, index) => (
+              <div key={index} className={styles.group}>
+                {el.options.map(inEl => (
+                  <label key={inEl.label}>
+                    <input checked={chosenAddon?.val === inEl.label} onClick={(e) => { handleAddonClick(e, { name: el.fieldName, val: inEl.label, price: inEl.price }) }} type="radio" name={el.name} value={inEl.label} />
+                    <span className={styles.checkbox} />
+                    <span>
+                      {inEl.label} <small>+&nbsp;{inEl.price}&nbsp;zł</small>
+                    </span>
+                  </label>
+                ))}
+              </div>
+            ))}
+          </div>
+        ) : null}
       </div>
       <div className={styles.flex}>
-        <Price salesPrice={chosenVariation?.price || product?.price} regularPrice={chosenVariation?.price || product?.regularPrice} />
-        <AddToCart variationId={chosenVariation?.productId} product={product} />
+        <Price salesPrice={prices.price} regularPrice={prices.regularPrice} />
+        <AddToCart chosenAddon={chosenAddon} variationId={chosenVariation?.productId} product={product} />
       </div>
     </div>
   )
