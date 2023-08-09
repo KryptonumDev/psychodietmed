275a3240f89c356111a162ede2a94bdff6ff0b72
Bogumił <hello@milewski.me>
2023-08-09 13:33:15 +0200

    merged

--- src/apollo/apolo-client.js
+++ src/apollo/apolo-client.js
@@ -1,67 +1,9 @@
-<<<<<<< HEAD
-import fetch from 'node-fetch';
-
 import {
   NextSSRInMemoryCache,
   NextSSRApolloClient,
 } from "@apollo/experimental-nextjs-app-support/ssr";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 import { createHttpLink } from "@apollo/client";
-=======
-import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from "@apollo/client";
-// import { getCookie } from '@/app/actions';
-
-/**
- * If we have a session token in localStorage, add it to the GraphQL request as a Session header.
- */
-export const middleware = new ApolloLink(async (operation, forward) => {
-  // If session data exist in local storage, set value as session header.
-  const session = (typeof window !== 'undefined') ? localStorage.getItem("woo-session") : null;
-  if (session) {
-    operation.setContext(({ headers = {} }) => ({
-      headers: {
-        "woocommerce-session": `Session ${session}`,
-      }
-    }));
-  }
-
-  return forward(operation);
-
-});
-
-/*
- * This catches the incoming session token and stores it in localStorage, for future GraphQL requests.
- */
-export const afterware = new ApolloLink((operation, forward) => {
-
-  return forward(operation).map(response => {
-
-    if (typeof window === 'undefined') {
-      return response;
-    }
-
-    // Check for session header and update session in local storage accordingly.
-    const context = operation.getContext();
-    const { response: { headers } } = context;
-    const session = headers.get("woocommerce-session");
-
-    if (session) {
-
-      // Remove session data if session destroyed.
-      if ("false" === session) {
-
-        localStorage.removeItem("woo-session");
-
-        // Update session new data if changed.
-      } else if (localStorage.getItem("woo-session") !== session) {
-
-        localStorage.setItem("woo-session", headers.get("woocommerce-session"));
-
-      }
-    }
-
-    return response;
->>>>>>> animations
 
 const { getClient } = registerApolloClient(() => {
   return new NextSSRApolloClient({
