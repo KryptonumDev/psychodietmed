cfc97195fa2f929a9b4d0f761f35099f7296032f
Bogumił <hello@milewski.me>
2023-08-10 16:52:04 +0200

    remove LoremIpsum

--- src/components/sections/flex-two-texts/index.js
+++ src/components/sections/flex-two-texts/index.js
@@ -5,7 +5,6 @@ import { Image } from "@/components/atoms/image"
 export default function TwoColumnFlexMultiText({ data: { contentFirstPart, contentSecondPart, image } }) {
   return (
     <section className={styles.wrapper}>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex itaque, cum fugit consectetur iste quae, dolor expedita impedit doloribus saepe atque delectus maxime molestiae soluta numquam exercitationem enim? Placeat maiores, tempore sit, delectus magni laudantium quaerat rerum aliquid fuga iure repellendus ea. Rem illo ipsam culpa ducimus? Cumque, dignissimos dolor? Ratione aspernatur quasi dignissimos amet illum explicabo rem, cum, autem iure quos quam? Nulla doloribus libero nemo, vitae deserunt beatae possimus ipsam totam necessitatibus assumenda. Esse ducimus autem consectetur tempore assumenda enim repellendus nesciunt voluptates quas necessitatibus, id repellat officiis quos dolorem sed in neque beatae repudiandae tenetur, facilis impedit!</p>
       <div className={styles.textFirst} dangerouslySetInnerHTML={{ __html: contentFirstPart }} />
       <div className={styles.textSecond} dangerouslySetInnerHTML={{ __html: contentSecondPart }} />
       <Image className={styles.image} src={image.mediaItemUrl} alt={image.altText} width={image.mediaDetails.width} height={image.mediaDetails.height} />
