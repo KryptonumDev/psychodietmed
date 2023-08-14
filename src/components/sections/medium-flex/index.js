2f566e6ff043f102d5bf50adea57762d1d017b04
kryptonumstudio <kryptonumstudio@gmail.com>
2023-08-14 11:42:57 +0200

    media fixes

--- src/components/sections/medium-flex/index.js
+++ src/components/sections/medium-flex/index.js
@@ -6,7 +6,7 @@ import { Image } from "@/components/atoms/image"
 export default function Flex({ data: { title, text, image } }) {
   return (
     <section className={styles.wrapper}>
-      <Image className={styles.image} alt={image.altText} src={image.mediaItemUrl} width={image.mediaDetails.width} height={image.mediaDetails.height} />
+      <Image aspectRatio={true} className={styles.image} alt={image.altText} src={image.mediaItemUrl} width={image.mediaDetails.width} height={image.mediaDetails.height} />
       <div>
         <h2 dangerouslySetInnerHTML={{ __html: removeWrap(title) }} />
         <div className={`${styles.text} gutenberg`} dangerouslySetInnerHTML={{ __html: text }} />
