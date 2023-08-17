91b636d7dc5723503293bad05d6192bd6a7a4c87
kryptonumstudio <kryptonumstudio@gmail.com>
2023-08-17 12:20:36 +0200

    comment calendar api

--- src/app/api/calendar/route.js
+++ src/app/api/calendar/route.js
@@ -1,19 +1,19 @@
-export async function GET(req) {
-  try {
-    const headers = {
-      'X-Tenant': process.env.CALENDESK_TENANT_NAME,
-      'X-API-Key': process.env.CALENDESK_API_KEY
-    };
-    fetch('https://api.calendesk.com/api/admin/availability-schedules', {
-      method: 'GET',
-      headers,
-      cache: 'no-cache'
-    })
-    .then(res => res.json())
-    .then(res => {
-      console.log(res)
-    })
-  } catch {
+// export async function GET(req) {
+//   try {
+//     const headers = {
+//       'X-Tenant': process.env.CALENDESK_TENANT_NAME,
+//       'X-API-Key': process.env.CALENDESK_API_KEY
+//     };
+//     fetch('https://api.calendesk.com/api/admin/availability-schedules', {
+//       method: 'GET',
+//       headers,
+//       cache: 'no-cache'
+//     })
+//     .then(res => res.json())
+//     .then(res => {
+//       console.log(res)
+//     })
+//   } catch {
 
-  }
-}
+//   }
+// }
