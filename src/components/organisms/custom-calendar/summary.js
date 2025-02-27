ed44c2265157bcc8814437ae68e6fb1f8ed34e49
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2025-02-27 09:37:38 +0100

    fix: correct day of week display in calendar summary

--- src/components/organisms/custom-calendar/summary.js
+++ src/components/organisms/custom-calendar/summary.js
@@ -4,13 +4,13 @@ import styles from './styles.module.scss'
 import Button from "@/components/atoms/button"
 
 const days = [
+  'Niedziela',
   'Poniedziałek',
   'Wtorek',
   'Środa',
   'Czwartek',
   'Piątek',
-  'Sobota',
-  'Niedziela'
+  'Sobota'
 ]
 
 export const CalendarSummary = ({ setPopupOpened, service, chosenTime, chosenDate }) => {
@@ -23,8 +23,8 @@ export const CalendarSummary = ({ setPopupOpened, service, chosenTime, chosenDat
         <p>godzina {chosenTime}</p>
       </div>
       <div>
-        <p>{(service.price/100)}&nbsp;zł / sesja {service.duration} minut</p>
-        <Button disabled={!chosenTime || !chosenDate} onClick={() => {setPopupOpened(true)}}>Umawiam wizytę</Button>
+        <p>{(service.price / 100)}&nbsp;zł / sesja {service.duration} minut</p>
+        <Button disabled={!chosenTime || !chosenDate} onClick={() => { setPopupOpened(true) }}>Umawiam wizytę</Button>
       </div>
     </div>
   )
