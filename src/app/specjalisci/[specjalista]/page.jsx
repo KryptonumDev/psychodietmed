cbf078badeb925f2ad4792fcb162c4fd1eea17c3
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2025-03-17 16:00:18 +0100

    fix: add leading zeros to month and day in date formatting

--- src/app/specjalisci/[specjalista]/page.jsx
+++ src/app/specjalisci/[specjalista]/page.jsx
@@ -1,46 +1,49 @@
-import { notFound } from "next/navigation"
-import Hero from "@/components/sections/hero-specialist"
-import Flex from "@/components/sections/specialist-flex"
-import FAQ from "@/components/sections/faq"
-import Reviews from "@/components/sections/specialist-reviews"
-import Specialists from "@/components/sections/specialists-slider"
-import { GET_SEO_SPECIALIST } from "../../../queries/specialist-seo"
-import { generetaSeo } from "../../../utils/genereate-seo"
-import Breadcrumbs from "@/components/sections/breadcrumbs"
-import { Fetch } from "../../../utils/fetch-query"
+import { notFound } from 'next/navigation';
+import Hero from '@/components/sections/hero-specialist';
+import Flex from '@/components/sections/specialist-flex';
+import FAQ from '@/components/sections/faq';
+import Reviews from '@/components/sections/specialist-reviews';
+import Specialists from '@/components/sections/specialists-slider';
+import { GET_SEO_SPECIALIST } from '../../../queries/specialist-seo';
+import { generetaSeo } from '../../../utils/genereate-seo';
+import Breadcrumbs from '@/components/sections/breadcrumbs';
+import { Fetch } from '../../../utils/fetch-query';
 
 export async function generateMetadata({ params }) {
-  return await generetaSeo(params.specjalista, '/specjalisci', GET_SEO_SPECIALIST, 'post')
+  return await generetaSeo(params.specjalista, '/specjalisci', GET_SEO_SPECIALIST, 'post');
 }
 
 export default async function Specjalista({ params }) {
-  const { data, faq, other } = await getData(params)
+  const { data, faq, other } = await getData(params);
   return (
     <main className="overflow" id="main">
-      <Breadcrumbs data={[{ page: 'Specjaliści', url: `/specjalisci` }, { page: data.title, url: `/specjalisci/${params.specjalista}` }]} />
-      <Hero
-        data={data}
-        specialistId={data.proffesional.specialistId}
-        serviceId={data.proffesional.serviceId}
+      <Breadcrumbs
+        data={[
+          { page: 'Specjaliści', url: `/specjalisci` },
+          { page: data.title, url: `/specjalisci/${params.specjalista}` },
+        ]}
       />
+      <Hero data={data} specialistId={data.proffesional.specialistId} serviceId={data.proffesional.serviceId} />
       <Flex
         content={data.proffesional.excerpt}
         diploms={data.proffesional.diploms}
         courses={data.proffesional.courses}
         certificates={data.proffesional.certificates}
       />
-      {data.proffesional.reviews && (
-        <Reviews data={data.proffesional.reviews} />
-      )}
+      {data.proffesional.reviews && <Reviews data={data.proffesional.reviews} />}
       <Specialists data={other} title={'Podobni specjaliści'} />
       <FAQ data={data.proffesional.faqSpecialist} />
     </main>
-  )
+  );
 }
 
 async function getData(params) {
   try {
-    const { body: { data: { specjalisci, specjalistaBy, page } } } = await Fetch({
+    const {
+      body: {
+        data: { specjalisci, specjalistaBy, page },
+      },
+    } = await Fetch({
       query: `
       query Pages($uri: String) {
         specjalisci(first: 100) {
@@ -137,23 +140,26 @@ async function getData(params) {
       revalidate: 600,
       variables: {
         uri: `${params.specjalista}`,
-      }
-    })
+      },
+    });
 
-    if (!specjalistaBy.id)
-      notFound()
+    if (!specjalistaBy.id) notFound();
 
     return {
       data: specjalistaBy,
-      other: specjalisci.nodes.filter(item => item.slug !== params.specjalista)
-    }
+      other: specjalisci.nodes.filter((item) => item.slug !== params.specjalista),
+    };
   } catch (error) {
-    notFound()
+    notFound();
   }
 }
 
 export async function generateStaticParams() {
-  const { body: { data: { specjalisci } } } = await Fetch({
+  const {
+    body: {
+      data: { specjalisci },
+    },
+  } = await Fetch({
     query: `
     query PostStaticParams {
       specjalisci(first: 100) {
@@ -163,10 +169,10 @@ export async function generateStaticParams() {
       }
     }
   `,
-  cache: 'no-cache'
-  })
+    cache: 'no-cache',
+  });
 
   return specjalisci.nodes.map(({ slug }) => ({
-    specjalista: slug
-  }))
+    specjalista: slug,
+  }));
 }
