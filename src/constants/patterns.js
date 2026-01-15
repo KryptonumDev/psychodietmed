2f39ecccb5671a2c2772a86550a9f26cb723090e
Bogumił Milewski <47248711+milewskibogumil@users.noreply.github.com>
2026-01-15 12:42:37 +0100

    feat: allow international phone numbers in all forms (1-4 digit country codes, 7-15 digits)

--- src/constants/patterns.js
+++ src/constants/patterns.js
@@ -1,4 +1,6 @@
 export const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 export const namePattern = /^[a-z ,.'-]+$/i
-export const phonePattern = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{3})/
-export const altPhonePattern = /^\+\d{2}\s?\d{3}\s?\d{3}\s?\d{3}$/g
+// International phone pattern: allows optional +, country code (1-4 digits), and number (with spaces/dashes/dots)
+// Minimum 7 digits, maximum 15 digits (ITU-T E.164 standard)
+export const phonePattern = /^\+?[0-9]{1,4}[-.\s]?(?:[0-9][-.\s]?){6,14}$/
+export const altPhonePattern = /^\+?[0-9]{1,4}[-.\s]?(?:[0-9][-.\s]?){6,14}$/
